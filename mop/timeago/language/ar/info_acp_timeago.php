2003ffee0908ebf89c52459a7a7a18ef96a0bda1
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-08-12 21:16:25 +0300

    Create info_acp_timeago.php

---
+++ mop/timeago/language/ar/info_acp_timeago.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * TimeAgo - LANGUAGE FILE
+ *
+ * This file contains the ( Arabic ) language definitions for
+ * the labels used in the ACP extensions tab
+ *
+ * PHP Version 5.4
+ *
+ * @category  PHP
+ * @package   timeago
+ * @author    MuhClaren
+ * @copyright 2015 (c) MOP
+ * @license   GNU General Public License v2
+ * 
+ * Translated By : Bassel Taha Alhitary - www.alhitary.net
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (defined('IN_PHPBB') === FALSE)
+{
+	exit;
+}
+
+if (empty($lang) || is_array($lang) === FALSE)
+{
+	$lang = [];
+}
+
+$lang = array_merge(
+	$lang,
+	[
+		// module category and section titles
+		'ACP_TIMEAGO_TITLE'            => 'التوقيت الماضي',
+		'ACP_TIMEAGO_GENERAL_SETTINGS' => 'الإعدادات',
+	]
+);
