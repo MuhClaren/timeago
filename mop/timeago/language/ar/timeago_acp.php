21e0f69cd1ee9bac7bec60d61af9b9311392ebf9
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-08-14 00:41:22 +0300

    Update timeago_acp.php

--- mop/timeago/language/ar/timeago_acp.php
+++ mop/timeago/language/ar/timeago_acp.php
@@ -14,7 +14,7 @@
  * @copyright 2015 (c) MOP
  * @license   GNU General Public License v2
  * 
- * Translated By : Bassel Taha Alhitary - www.alhitary.net
+ * Translated By : Bassel Taha Alhitary - www.alhitary.net.
  */
 
 /**
@@ -33,19 +33,20 @@ if (empty($lang) || is_array($lang) === FALSE)
 $lang = array_merge(
 	$lang,
 	[
-		// common lang variables
-		'CHARS'                       => 'حروف',
-		'TA_SECOND'                   => 'ثانية',
-		'TA_MINUTE'                   => 'دقيقة',
-		'TA_HOUR'                     => 'ساعة',
-		'TA_DAY'                      => 'يوم',
-		'TA_WEEK'                     => 'أسبوع',
-		'TA_MONTH'                    => 'شهر',
-		'TA_YEAR'                     => 'سنة',
-		'TA_DECADE'                   => '10 سنوات',
+		// Set the plurals - <key> => 'plural string' where <key> is the unit count, such as number of days
+		'TA_SECOND'                   => [0 => 'ثواني', 1 => 'ثانية', 2 => 'ثواني', 3 => 'ثواني', 4 => 'ثواني',],
+		'TA_MINUTE'                   => [0 => 'دقائق', 1 => 'دقيقة', 2 => 'دقائق', 3 => 'دقائق', 4 => 'دقائق',],
+		'TA_HOUR'                     => [0 => 'ساعات', 1 => 'ساعة', 2 => 'ساعات', 3 => 'ساعات', 4 => 'ساعات',],
+		'TA_DAY'                      => [0 => 'أيام', 1 => 'يوم', 2 => 'أيام', 3 => 'أيام', 4 => 'أيام',],
+		'TA_WEEK'                     => [0 => 'أسابيع', 1 => 'أسبوع', 2 => 'أسابيع', 3 => 'أسابيع', 4 => 'أسابيع',],
+		'TA_MONTH'                    => [0 => 'شهور', 1 => 'شهر', 2 => 'شهور', 3 => 'شهور', 4 => 'شهور',],
+		'TA_YEAR'                     => [0 => 'سنوات', 1 => 'سنة', 2 => 'سنوات', 3 => 'سنوات', 4 => 'سنوات',],
+		'TA_DECADE'                   => [0 => 'عقود', 1 => 'عقد', 2 => 'عقود', 3 => 'عقود', 4 => 'عقود',],
+		'TA_AGO'                      => 'مُنذ',
 		'TA_OFF'                      => 'لا',
 		'TA_SHORT'                    => 'قصير ( مُنذ 1 سنة )',
 		'TA_MEDIUM'                   => 'متوسط ( مُنذ 1 سنة 2 شهور )',
+		'TA_FULL'                     => 'كامل ( مُنذ 1 سنة 2 شهور 3 أيام )',
 		// general settings
 		'TA_GENERAL_SETTINGS'         => 'الإعدادات',
 		'TA_GENERAL_SETTINGS_EXPLAIN' => 'ضبط الإعدادات',
@@ -56,9 +57,8 @@ $lang = array_merge(
 		'TA_VIEWFORUM_EXPLAIN'        => 'تطبيق التوقيت الماضي في قائمة المواضيع بصفحة المنتدى',
 		'TA_VIEWTOPIC'                => 'صفحة الموضوع ',
 		'TA_VIEWTOPIC_EXPLAIN'        => 'تطبيق التوقيت الماضي في كل مُشاركة ( صفحة الموضوع )',
-		'TA_FULL'                     => 'كامل ( مُنذ 1 سنة 2 شهور 3 أيام )',
-		'TA_EXTENDED'                 => 'إضافة',
-		'TA_EXTENDED_EXPLAIN'         => 'إضافة صيغة التوقيت الإفتراضي للمنتدى في نهاية التوقيت الماضي.',
+		'TA_EXTENDED'                 => 'بالإضافة إلى ',
+		'TA_EXTENDED_EXPLAIN'         => 'إضافة صيغة التوقيت الإفتراضي للمنتدى بعد التوقيت الماضي.',
 		'TA_EXTENDED_EXAMPLE'         => '( مثال : مُنذ 9 ساعات ( السبت أغسطس 08 , 2015 11:57 ص ) )',
 		'TA_DETAIL'                   => 'مستوى التفاصيل',
 	]
