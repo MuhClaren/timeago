80b1f99b132fc9407155de2126d643fac4eec211
edipdincer <edipdincer@gmail.com>
2015-09-17 09:54:44 +0300

    Create info_acp_timeago.php

---
+++ language/tr/info_acp_timeago.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * TimeAgo - LANGUAGE FILE.
+	 *
+	 * This file contains the (Turkish) language definitions for
+	 * the labels used in the ACP extensions tab
+	 *
+	 * PHP Version 5.4
+	 *
+	 * @category  PHP
+	 *
+	 * @author    MuhClaren
+	 * @copyright 2015 (c) MOP
+	 * @license   GNU General Public License v2
+	 */
+	/**
+	 * DO NOT CHANGE.
+	 */
+	if (defined('IN_PHPBB') === false)
+	{
+		exit;
+	}
+	if (empty($lang) || is_array($lang) === false)
+	{
+		$lang = [];
+	}
+	$lang = array_merge(
+		$lang,
+		[
+			// module category and section titles
+			'ACP_TIMEAGO_TITLE'            => 'TimeAgo',
+			'ACP_TIMEAGO_GENERAL_SETTINGS' => 'Genel Ayarlar',
+		]
+	);
